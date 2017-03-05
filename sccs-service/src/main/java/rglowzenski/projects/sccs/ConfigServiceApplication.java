8f6793758369fcaef1b0661f8e925415c5c070d1
rich <rich@MEDIA>
2017-03-05 17:46:34 -0500

    refactoring

--- sccs-service/src/main/java/rglowzenski/projects/sccs/ConfigServiceApplication.java
+++ sccs-service/src/main/java/rglowzenski/projects/sccs/ConfigServiceApplication.java
@@ -1,4 +1,4 @@
-package glowzenski.projects.sccs;
+package rglowzenski.projects.sccs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
